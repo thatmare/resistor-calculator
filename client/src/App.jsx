28cdd8de0ae27c75753138681eab69226d0659ec
Marissa Vargas <marissa.lorena@hotmail.com>
2023-09-22 16:06:38 -0600

    build: tailwind install and config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,10 @@
-import { useState, useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [backendData, setBackendData] = useState([{}]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api")
-      .then((res) => res.json()) // Analiza la respuesta JSON aquí
-      .then((data) => {
-        setBackendData(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }, []);
-  
+function App() {  
 
   return (
     <div>
-      hola
+      
     </div>
   )
 }
